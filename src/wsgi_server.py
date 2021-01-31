b48e616f2b44c376aa95e02db7ca2bd39ee1a58d
Danton Pimentel <82546+dantonpimentel@users.noreply.github.com>
2021-01-31 13:51:46 -0500

    #7 Adding GUnicorn Wrapper

---
+++ src/wsgi_server.py
@@ -0,0 +1,18 @@
+from copy import deepcopy
+
+from gunicorn.app.base import BaseApplication
+
+class WSGIWrapper(BaseApplication):
+    def __init__(self, app, config):
+        self.options = deepcopy(config)
+        self.application = app
+        super().__init__()
+
+    def load_config(self):
+        config = {key: value for key, value in self.options.items()
+                  if key in self.cfg.settings and value is not None}
+        for key, value in config.items():
+            self.cfg.set(key.lower(), value)
+
+    def load(self):
+        return self.application
